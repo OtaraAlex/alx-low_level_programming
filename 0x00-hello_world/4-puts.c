faf2c95bfc7fd729d81b76b87b813ddce46669e0
OtaraAlex <alexotara61@gmail.com>
2022-09-08 12:46:50 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - printimg
+ * main - Printing
  *
  * Return: Always 0 (Successful)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	Return (0)
+	puts("\"Programming is like building a multilingual
+puzzle");
+    return (0)
 }
